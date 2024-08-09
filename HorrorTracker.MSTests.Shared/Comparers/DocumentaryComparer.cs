300f8a931f04e78b88bfb573e0220c1e18db935c
James McKinney <mckinneyjames1995@gmail.com>
2024-08-08 21:38:28 -0400

    Create DocumentaryComparer for tests. More tests for DocumentaryRepository tests.

---
+++ HorrorTracker.MSTests.Shared/Comparers/DocumentaryComparer.cs
@@ -0,0 +1,32 @@
+﻿using HorrorTracker.Data.Models;
+using System.Collections;
+
+namespace HorrorTracker.MSTests.Shared.Comparers
+{
+    public class DocumentaryComparer : IComparer
+    {
+        public int Compare(object? x, object? y)
+        {
+            var documentaryOne = x as Documentary;
+            var documentaryTwo = y as Documentary;
+
+            if (documentaryOne == null && documentaryTwo == null) return 0;
+            if (documentaryOne == null) return -1;
+            if (documentaryTwo == null) return 1;
+
+            int result = documentaryOne.Id.CompareTo(documentaryTwo.Id);
+            if (result != 0) return result;
+
+            result = string.Compare(documentaryOne.Title, documentaryTwo.Title, StringComparison.Ordinal);
+            if (result != 0) return result;
+
+            result = documentaryOne.TotalTime.CompareTo(documentaryTwo.TotalTime);
+            if (result != 0) return result;
+
+            result = documentaryOne.ReleaseYear.CompareTo(documentaryTwo.ReleaseYear);
+            if (result != 0) return result;
+
+            return documentaryOne.Watched.CompareTo(documentaryTwo.Watched);
+        }
+    }
+}
--- HorrorTracker.MSTests/Data/Repositories/DocumentaryRepositoryTests.cs
+++ HorrorTracker.MSTests/Data/Repositories/DocumentaryRepositoryTests.cs
@@ -1,9 +1,12 @@
 ﻿using HorrorTracker.Data.Constants.Queries;
+using HorrorTracker.Data.Models;
 using HorrorTracker.Data.PostgreHelpers.Interfaces;
 using HorrorTracker.Data.Repositories;
 using HorrorTracker.MSTests.Shared;
+using HorrorTracker.MSTests.Shared.Comparers;
 using HorrorTracker.Utilities.Logging.Interfaces;
 using Moq;
+using System.Data;
 using System.Diagnostics.CodeAnalysis;
 
 namespace HorrorTracker.MSTests.Data.Repositories
@@ -84,6 +87,110 @@ namespace HorrorTracker.MSTests.Data.Repositories
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
             _loggerVerifier.VerifyErrorMessage($"Error adding documentary '{documentary.Title}'.", Messages.ExceptionMessage);
+        }
+
+        [TestMethod]
+        public void Delete_WhenSuccessful_ShouldReturnMessageAndLogMessage()
+        {
+            // Arrange
+            var documentaryId = Fixtures.Documentary().Id;
+            var expectedMessage = $"Documentary with ID '{documentaryId}' deleted successfully.";
+            _mockSetupManager.SetupExecuteNonQueryDatabaseCommand(DocumentaryQueries.DeleteDocumentary, 1);
+
+            // Act
+            var actualMessage = _repository.Delete(documentaryId);
+
+            // Assert
+            Assert.AreEqual(expectedMessage, actualMessage);
+            _loggerVerifier.VerifyLoggerInformationMessages(Messages.DatabaseOpened, actualMessage);
+        }
+
+        [TestMethod]
+        public void Delete_WhenDocumentaryDoesNotExist_ShouldReturnCorrectMessageAndLogMessages()
+        {
+            // Arrange
+            var documentaryId = Fixtures.Documentary().Id;
+            var expectedMessage = "Deleting documentary was not successful.";
+            _mockSetupManager.SetupExecuteNonQueryDatabaseCommand(DocumentaryQueries.DeleteDocumentary, 0);
+
+            // Act
+            var actualMessage = _repository.Delete(documentaryId);
+
+            // Assert
+            Assert.AreEqual(expectedMessage, actualMessage);
+            _loggerVerifier.VerifyLoggerInformationMessages(Messages.DatabaseOpened);
+        }
+
+        [TestMethod]
+        public void Delete_WhenExceptionIsThrown_ShouldReturnCorrectMessageAndLogError()
+        {
+            // Arrange
+            var documentaryId = Fixtures.Documentary().Id;
+            var expectedMessage = $"Error deleting documentary with ID '{documentaryId}'.";
+            _mockSetupManager.SetupException(Messages.ExceptionMessage);
+
+            // Act
+            var actualMessage = _repository.Delete(documentaryId);
+
+            // Assert
+            Assert.AreEqual(expectedMessage, actualMessage);
+            _loggerVerifier.VerifyErrorMessage(actualMessage, Messages.ExceptionMessage);
+        }
+
+        [TestMethod]
+        public void GetAll_WhenSuccessful_ShouldReturnListOfDocumentariesAndLogMessage()
+        {
+            // Arrange
+            var documentaryOne = Fixtures.Documentary();
+            var documentaryTwo = Fixtures.Documentary();
+            var expectedResult = new List<Documentary>
+            {
+                documentaryOne,
+                documentaryTwo
+            };
+
+            SetupMockReaderForDocumentaries(documentaryOne, documentaryTwo);
+
+            // Act
+            var actualResult = _repository.GetAll().ToList();
+
+            // Assert
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(expectedResult.Count, actualResult.Count);
+            CollectionAssert.AreEqual(expectedResult, actualResult, new DocumentaryComparer());
+            _loggerVerifier.VerifyLoggerInformationMessages(Messages.DatabaseOpened, "Successfully retrieved all of the documentaries.");
+        }
+
+        [TestMethod]
+        public void GetAll_WhenExceptionIsThrown_ShouldLogErrorMessageAndReturnEmptyList()
+        {
+            // Arrange
+            _mockSetupManager.SetupException(Messages.ExceptionMessage);
+
+            // Act
+            var result = _repository.GetAll().ToList();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+            _loggerVerifier.VerifyErrorMessage("Error fetching all of the documentaries.", Messages.ExceptionMessage);
+        }
+
+        private void SetupMockReaderForDocumentaries(Documentary documentaryOne, Documentary documentaryTwo)
+        {
+            var mockReader = new Mock<IDataReader>();
+            _mockDatabaseConnection.Setup(c => c.CreateCommand()).Returns(_mockDatabaseCommand.Object);
+            _mockDatabaseCommand.Setup(cmd => cmd.ExecuteReader()).Returns(mockReader.Object);
+            mockReader.SetupSequence(reader => reader.Read())
+                      .Returns(true)
+                      .Returns(true)
+                      .Returns(false);
+
+            mockReader.SetupSequence(reader => reader.GetInt32(0)).Returns(documentaryOne.Id).Returns(documentaryTwo.Id);
+            mockReader.SetupSequence(reader => reader.GetString(1)).Returns(documentaryOne.Title).Returns(documentaryTwo.Title);
+            mockReader.SetupSequence(reader => reader.GetDecimal(2)).Returns(documentaryOne.TotalTime).Returns(documentaryTwo.TotalTime);
+            mockReader.SetupSequence(reader => reader.GetInt32(3)).Returns(documentaryOne.ReleaseYear).Returns(documentaryTwo.ReleaseYear);
+            mockReader.SetupSequence(reader => reader.GetBoolean(4)).Returns(documentaryOne.Watched).Returns(documentaryTwo.Watched);
         }
     }
 }
