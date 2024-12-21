e7a6abc141f45390c841cfcaf95a0794c34d0071
James McKinney <mckinneyjames1995@gmail.com>
2024-12-20 20:09:10 -0500

    Add supernatural subgenre enum.

---
+++ HorrorTracker.Data/Enumerations/Subgenres/SupernaturalSubgenre.cs
@@ -0,0 +1,14 @@
+﻿namespace HorrorTracker.Data.Enumerations.Subgenres
+{
+    /// <summary>
+    /// The <see cref="SupernaturalSubgenre"/> enumeration.
+    /// </summary>
+    public enum SupernaturalSubgenre
+    {
+        Ghosts,
+        Demons,
+        Possession,
+        Witches,
+        Curses
+    }
+}
