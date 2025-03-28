1441714ef920b8d65198465bebfcc9fdc5b423bf
James McKinney <mckinneyjames1995@gmail.com>
2025-03-27 21:55:49 -0400

    Make constructor a primary constructor.

--- HorrorTracker.Data/PostgreHelpers/DatabaseConnection.cs
+++ HorrorTracker.Data/PostgreHelpers/DatabaseConnection.cs
@@ -8,22 +8,17 @@ namespace HorrorTracker.Data.PostgreHelpers
     /// The <see cref="DatabaseConnection"/> class.
     /// </summary>
     /// <seealso cref="IDatabaseConnection"/>
+    /// <remarks>
+    /// Initializes a new instance of the <see cref="DatabaseConnection"/> class.
+    /// </remarks>
+    /// <param name="connectionString">The connection string.</param>
     [ExcludeFromCodeCoverage]
-    public class DatabaseConnection : IDatabaseConnection
+    public class DatabaseConnection(string? connectionString) : IDatabaseConnection
     {
         /// <summary>
         /// The connection to a Postgre server.
         /// </summary>
-        private readonly NpgsqlConnection _connection;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DatabaseConnection"/> class.
-        /// </summary>
-        /// <param name="connectionString">The connection string.</param>
-        public DatabaseConnection(string? connectionString)
-        {
-            _connection = new NpgsqlConnection(connectionString);
-        }
+        private readonly NpgsqlConnection _connection = new NpgsqlConnection(connectionString);
 
         /// <inheritdoc/>
         public void Open()
